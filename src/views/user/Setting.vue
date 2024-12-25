240f61744ea9ab72d82e5f2750f5a133ae83a47c
FuyukiVila <1642421711@qq.com>
2024-12-25 19:58:32 +0800

    update

--- src/views/user/Setting.vue
+++ src/views/user/Setting.vue
@@ -8,16 +8,16 @@
         <div class="column is-full">
           <el-form ref="form" :label-position="labelPosition" :model="user" label-width="100px">
             <el-form-item label="账号">
-              <el-input v-model="user.username" disabled/>
-            </el-form-item>
-            <el-form-item :rules="rules.alias" label="昵称" prop="alias">
-              <el-input v-model="user.alias"/>
-            </el-form-item>
-            <el-form-item :rules="rules.bio" label="简介" prop="bio">
-              <el-input v-model="user.bio"/>
+              <el-input v-model="user.username" disabled />
             </el-form-item>
             <el-form-item :rules="rules.email" label="邮箱" prop="email">
-              <el-input v-model="user.email"/>
+              <el-input v-model="user.email" disabled />
+            </el-form-item>
+            <el-form-item :rules="rules.alias" label="昵称" prop="alias">
+              <el-input v-model="user.alias" />
+            </el-form-item>
+            <el-form-item :rules="rules.bio" label="简介" prop="bio">
+              <el-input v-model="user.bio" />
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="submitForm('form')">提交</el-button>
@@ -54,10 +54,6 @@ export default {
         ],
         bio: [
           {max: 100, message: '最多100个字符', trigger: 'blur'}
-        ],
-        email: [
-          {required: true, message: '请输入邮箱地址', trigger: 'blur'},
-          {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
         ],
       }
     }
